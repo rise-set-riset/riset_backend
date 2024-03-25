286a4e1db3b96c7e2e392836f0def5067f470361
sang hyeok Lee <saing0813@naver.com>
2024-03-25 17:44:23 +0900

    first commit

---
+++ src/main/java/com/github/riset_backend/RisetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.riset_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RisetBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/riset_backend/RisetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.riset_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RisetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
