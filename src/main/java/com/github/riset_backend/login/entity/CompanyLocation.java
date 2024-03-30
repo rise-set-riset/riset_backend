c580460f2f370ae7ef612b064fd01ae33b081f08
Min-Seok-Kim <70834609+Min-Seok-Kim@users.noreply.github.com>
2024-03-30 19:52:23 +0900

    feat: 기본 세팅
    엔티티 및 레포지토리 추가

---
+++ src/main/java/com/github/riset_backend/login/entity/CompanyLocation.java
@@ -0,0 +1,32 @@
+package com.github.riset_backend.login.entity;
+
+
+import com.github.riset_backend.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "company_location")
+public class CompanyLocation extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "location_no")
+    private Long locationNo;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
+
+    @Column(name = "commnet")
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "company_no", referencedColumnName = "company_no")
+    private Company company;
+}
