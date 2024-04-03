55866f6e46e7d2f32c37b7c4c6643a1c31f5a520
sang hyeok Lee <saing0813@naver.com>
2024-04-03 14:38:33 +0900

    hotfix: cors config

---
+++ src/main/java/com/github/riset_backend/global/config/cors/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.github.riset_backend.global.config.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings (CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("Authorization", "Cache-Control", "Content-type")
+                .allowCredentials(true);
+    }
+}
