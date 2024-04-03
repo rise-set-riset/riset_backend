7f992f05590538eb20dc52337f6c0461dc90a66a
sang hyeok Lee <saing0813@naver.com>
2024-04-03 21:51:52 +0900

    fix

--- src/main/java/com/github/riset_backend/global/config/cors/CorsConfig.java
+++ src/main/java/com/github/riset_backend/global/config/cors/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.github.riset_backend.global.config.cors;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,9 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings (CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*");
-//                .allowedHeaders("Content-type");
-//                .allowCredentials(true);
+                .allowedOrigins("*").
+                allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PATCH.name(), HttpMethod.PUT.name(), HttpMethod.OPTIONS.name());
     }
 }
