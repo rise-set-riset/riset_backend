3958772ab6b188638bc6cad0cbf55758a77bb85c
sang hyeok Lee <saing0813@naver.com>
2024-04-03 23:16:01 +0900

    fix

--- src/main/java/com/github/riset_backend/global/config/cors/CorsConfig.java
+++ src/main/java/com/github/riset_backend/global/config/cors/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings (CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PATCH.name(), HttpMethod.PUT.name(), HttpMethod.OPTIONS.name());
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PATCH.name(), HttpMethod.PUT.name(), HttpMethod.OPTIONS.name())
+                .allowedHeaders("*");
     }
 }
