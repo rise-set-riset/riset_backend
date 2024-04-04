5c2fb6b0d9761560be5c98eb333c8b71e11574fa
sang hyeok Lee <saing0813@naver.com>
2024-04-04 18:43:45 +0900

    fix

--- src/main/java/com/github/riset_backend/login/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/github/riset_backend/login/auth/filter/JwtAuthenticationFilter.java
@@ -1,57 +1,57 @@
-package com.github.riset_backend.login.auth.filter;
-
-import com.github.riset_backend.global.config.auth.JwtTokenProvider;
-import com.github.riset_backend.global.config.exception.BusinessException;
-import com.github.riset_backend.global.config.exception.ErrorCode;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.RedisConnectionFailureException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
-import org.springframework.web.filter.OncePerRequestFilter;
-import java.io.IOException;
-
-@Component
-@RequiredArgsConstructor
-@Slf4j
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private static final String AUTHORIZATION_HEADER = "Authorization";
-    private static final String BEARER_TYPE = "Bearer";
-
-    private final JwtTokenProvider jwtTokenProvider;
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String requestURI = request.getRequestURI();
-        // 1. Request Header 에서 토큰을 꺼냄
-        String jwt = resolveToken(request);
-
-
-        // 2. validateToken 으로 토큰 유효성 검사, 정상 토큰이면 해당 토큰으로 Authentication을 가져와서 SecurityContext에 저장
-        if(StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
-            Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            log.info("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
-
-        }else {
-            log.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
-        }
-
-
-        filterChain.doFilter(request, response);
-    }
-
-    private String resolveToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_TYPE)) {
-            return bearerToken.substring(7);
-        }
-        return null;
-    }
-}
+//package com.github.riset_backend.login.auth.filter;
+//
+//import com.github.riset_backend.global.config.auth.JwtTokenProvider;
+//import com.github.riset_backend.global.config.exception.BusinessException;
+//import com.github.riset_backend.global.config.exception.ErrorCode;
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.data.redis.RedisConnectionFailureException;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.stereotype.Component;
+//import org.springframework.util.StringUtils;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//import java.io.IOException;
+//
+//@Component
+//@RequiredArgsConstructor
+//@Slf4j
+//public class JwtAuthenticationFilter extends OncePerRequestFilter {
+//    private static final String AUTHORIZATION_HEADER = "Authorization";
+//    private static final String BEARER_TYPE = "Bearer";
+//
+//    private final JwtTokenProvider jwtTokenProvider;
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+//        String requestURI = request.getRequestURI();
+//        // 1. Request Header 에서 토큰을 꺼냄
+//        String jwt = resolveToken(request);
+//
+//
+//        // 2. validateToken 으로 토큰 유효성 검사, 정상 토큰이면 해당 토큰으로 Authentication을 가져와서 SecurityContext에 저장
+//        if(StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
+//            Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
+//            SecurityContextHolder.getContext().setAuthentication(authentication);
+//            log.info("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
+//
+//        }else {
+//            log.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
+//        }
+//
+//
+//        filterChain.doFilter(request, response);
+//    }
+//
+//    private String resolveToken(HttpServletRequest request) {
+//        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+//        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_TYPE)) {
+//            return bearerToken.substring(7);
+//        }
+//        return null;
+//    }
+//}
--- src/main/java/com/github/riset_backend/login/auth/service/AuthService.java
+++ src/main/java/com/github/riset_backend/login/auth/service/AuthService.java
@@ -1,99 +1,99 @@
-package com.github.riset_backend.login.auth.service;
-
-
+//package com.github.riset_backend.login.auth.service;
+//
+//
+////import com.github.riset_backend.global.config.auth.JwtTokenProvider;
 //import com.github.riset_backend.global.config.auth.JwtTokenProvider;
-import com.github.riset_backend.global.config.auth.JwtTokenProvider;
-import com.github.riset_backend.global.config.exception.BusinessException;
-import com.github.riset_backend.global.config.exception.ErrorCode;
-import com.github.riset_backend.login.auth.dto.RequestLoginDto;
-import com.github.riset_backend.login.auth.dto.RequestSignUpDto;
-import com.github.riset_backend.login.auth.dto.TokenDto;
-import com.github.riset_backend.login.employee.entity.Employee;
-import com.github.riset_backend.login.employee.repository.EmployeeRepository;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
-
-
-@Service
-@Slf4j
-@RequiredArgsConstructor
-public class AuthService {
-    private final EmployeeRepository employeeRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final RedisTemplate<String, String> redisTemplate;
-
-    @Transactional
-    public ResponseEntity<String> employeeSignup(RequestSignUpDto requestSignUpDto) {
-        if (employeeRepository.existsByEmployeeId(requestSignUpDto.id())) {
-            throw new BusinessException(ErrorCode.DUPLICATED_LOGIN_ID);
-        }
-
-        Employee employee = Employee.builder()
-                .employeeId(requestSignUpDto.id())
-                .password(passwordEncoder.encode(requestSignUpDto.password()))
-                .name(requestSignUpDto.name())
-                .phoneNumber(requestSignUpDto.phone())
-                .build();
-
-        employeeRepository.save(employee);
-
-        return ResponseEntity.ok().body("회원가입이 완료되었습니다.");
-    }
-
-    @Transactional
-    public ResponseEntity<?> employeeLogin(RequestLoginDto requestLoginDto, HttpServletResponse httpServletResponse) {
-        Employee employee = employeeRepository.findByEmployeeId(requestLoginDto.id()).isEmpty() ? null : employeeRepository.findByEmployeeId(requestLoginDto.id()).get();
-
-        if (employee == null) {
-            throw (new BusinessException(ErrorCode.CHECK_LOGIN_ID_OR_PASSWORD));
-        }
-
-        Map<String, String> response = new HashMap<>();
-
-        try {
-            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(requestLoginDto.id(), requestLoginDto.password());
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-
-            if (redisTemplate.opsForValue().get("logout: " + requestLoginDto.id()) != null) {
-                redisTemplate.delete("logout: " + requestLoginDto.id());
-            }
-
-            if(!passwordEncoder.matches(requestLoginDto.password(), employee.getPassword()))
-                throw new BusinessException(ErrorCode.NOT_EQUAL_PASSWORD);
-            TokenDto tokenDto = jwtTokenProvider.generateToken(authenticationToken);
-
-            redisTemplate.opsForValue().set(requestLoginDto.id(), tokenDto.getAccesstoken(), Duration.ofHours(1L));
-            redisTemplate.opsForValue().set("RF: " + requestLoginDto.id(), tokenDto.getRefreshtoken(), Duration.ofHours(3L));
-
-            response.put("http_status", HttpStatus.OK.toString());
-            response.put("message", "로그인 되었습니다");
-            response.put("token_type", "Bearer");
-            response.put("access_token", tokenDto.getAccesstoken());
-            response.put("refresh_token", tokenDto.getRefreshtoken());
-
-            return ResponseEntity.ok(response);
-
-        } catch (BadCredentialsException e) {
-            e.printStackTrace();
-            response.put("message", "잘못된 자격 증명입니다");
-            response.put("http_status", HttpStatus.UNAUTHORIZED.toString());
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
-        }
-    }
-}
+//import com.github.riset_backend.global.config.exception.BusinessException;
+//import com.github.riset_backend.global.config.exception.ErrorCode;
+//import com.github.riset_backend.login.auth.dto.RequestLoginDto;
+//import com.github.riset_backend.login.auth.dto.RequestSignUpDto;
+//import com.github.riset_backend.login.auth.dto.TokenDto;
+//import com.github.riset_backend.login.employee.entity.Employee;
+//import com.github.riset_backend.login.employee.repository.EmployeeRepository;
+//import jakarta.servlet.http.HttpServletResponse;
+//import jakarta.transaction.Transactional;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.BadCredentialsException;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//import java.time.Duration;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//
+//@Service
+//@Slf4j
+//@RequiredArgsConstructor
+//public class AuthService {
+//    private final EmployeeRepository employeeRepository;
+//    private final PasswordEncoder passwordEncoder;
+//    private final JwtTokenProvider jwtTokenProvider;
+//    private final RedisTemplate<String, String> redisTemplate;
+//
+//    @Transactional
+//    public ResponseEntity<String> employeeSignup(RequestSignUpDto requestSignUpDto) {
+//        if (employeeRepository.existsByEmployeeId(requestSignUpDto.id())) {
+//            throw new BusinessException(ErrorCode.DUPLICATED_LOGIN_ID);
+//        }
+//
+//        Employee employee = Employee.builder()
+//                .employeeId(requestSignUpDto.id())
+//                .password(passwordEncoder.encode(requestSignUpDto.password()))
+//                .name(requestSignUpDto.name())
+//                .phoneNumber(requestSignUpDto.phone())
+//                .build();
+//
+//        employeeRepository.save(employee);
+//
+//        return ResponseEntity.ok().body("회원가입이 완료되었습니다.");
+//    }
+//
+//    @Transactional
+//    public ResponseEntity<?> employeeLogin(RequestLoginDto requestLoginDto, HttpServletResponse httpServletResponse) {
+//        Employee employee = employeeRepository.findByEmployeeId(requestLoginDto.id()).isEmpty() ? null : employeeRepository.findByEmployeeId(requestLoginDto.id()).get();
+//
+//        if (employee == null) {
+//            throw (new BusinessException(ErrorCode.CHECK_LOGIN_ID_OR_PASSWORD));
+//        }
+//
+//        Map<String, String> response = new HashMap<>();
+//
+//        try {
+//            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(requestLoginDto.id(), requestLoginDto.password());
+//            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+//
+//            if (redisTemplate.opsForValue().get("logout: " + requestLoginDto.id()) != null) {
+//                redisTemplate.delete("logout: " + requestLoginDto.id());
+//            }
+//
+//            if(!passwordEncoder.matches(requestLoginDto.password(), employee.getPassword()))
+//                throw new BusinessException(ErrorCode.NOT_EQUAL_PASSWORD);
+//            TokenDto tokenDto = jwtTokenProvider.generateToken(authenticationToken);
+//
+//            redisTemplate.opsForValue().set(requestLoginDto.id(), tokenDto.getAccesstoken(), Duration.ofHours(1L));
+//            redisTemplate.opsForValue().set("RF: " + requestLoginDto.id(), tokenDto.getRefreshtoken(), Duration.ofHours(3L));
+//
+//            response.put("http_status", HttpStatus.OK.toString());
+//            response.put("message", "로그인 되었습니다");
+//            response.put("token_type", "Bearer");
+//            response.put("access_token", tokenDto.getAccesstoken());
+//            response.put("refresh_token", tokenDto.getRefreshtoken());
+//
+//            return ResponseEntity.ok(response);
+//
+//        } catch (BadCredentialsException e) {
+//            e.printStackTrace();
+//            response.put("message", "잘못된 자격 증명입니다");
+//            response.put("http_status", HttpStatus.UNAUTHORIZED.toString());
+//            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
+//        }
+//    }
+//}
