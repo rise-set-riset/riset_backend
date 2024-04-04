5a3a66dd87eae6d8c89a7feff626f30fc65a32a9
sang hyeok Lee <saing0813@naver.com>
2024-04-04 16:14:52 +0900

    feat: add upload config

---
+++ src/main/java/com/github/riset_backend/global/config/uplaod/MultipartJackson2HttpMessageConverter.java
@@ -0,0 +1,30 @@
+package com.github.riset_backend.global.config.uplaod;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+
+import java.lang.reflect.Type;
+
+@Configuration
+public class MultipartJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+    public MultipartJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+        super(objectMapper, MediaType.APPLICATION_OCTET_STREAM);
+    }
+
+    @Override
+    public boolean canWrite(Class<?> clazz, MediaType mediaType) {
+        return false;
+    }
+
+    @Override
+    public boolean canWrite(Type type, Class<?> clazz, MediaType mediaType) {
+        return false;
+    }
+
+    @Override
+    protected boolean canWrite(MediaType mediaType) {
+        return false;
+    }
+}
