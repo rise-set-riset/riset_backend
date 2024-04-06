619af060622cb5e06f7f09640f7680555059a7c4
sang hyeok Lee <saing0813@naver.com>
2024-04-06 19:29:32 +0900

    feat: FavoriteController

---
+++ src/main/java/com/github/riset_backend/writeBoard/favorite/controller/FavoriteController.java
@@ -0,0 +1,35 @@
+package com.github.riset_backend.writeBoard.favorite.controller;
+
+import com.github.riset_backend.global.config.auth.custom.CustomUserDetails;
+import com.github.riset_backend.writeBoard.favorite.dto.FavoriteResponseDto;
+import com.github.riset_backend.writeBoard.favorite.service.FavoriteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/board/favorite")
+public class FavoriteController {
+
+    private final FavoriteService favoriteService;
+
+    @GetMapping()
+    public ResponseEntity<List<FavoriteResponseDto>> getAllFavoriteBoard (@AuthenticationPrincipal CustomUserDetails customUserDetails,
+                                                                          @RequestParam(defaultValue = "0") int page,
+                                                                          @RequestParam(defaultValue = "10") int size) {
+        List<FavoriteResponseDto> favorites = favoriteService.getAllFavoriteBoard(customUserDetails.getEmployee().getEmployeeNo(), page, size);
+        return ResponseEntity.ok(favorites);
+    }
+
+    @PostMapping("/{boardNo}")
+    public ResponseEntity<FavoriteResponseDto> createFavoriteBoard (@PathVariable Long boardNo, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        FavoriteResponseDto favorite = favoriteService.createFavoriteBoard(boardNo, customUserDetails.getEmployee().getEmployeeNo());
+        return ResponseEntity.ok(favorite);
+    }
+
+}
+
