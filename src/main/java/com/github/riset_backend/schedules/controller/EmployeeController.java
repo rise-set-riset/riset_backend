e82303c32fde4cd5e344ba7dac0694715f98b2a2
sang hyeok Lee <saing0813@naver.com>
2024-04-07 14:54:46 +0900

    hotfix: rename employeeSchedulesService in EmployeeController

--- src/main/java/com/github/riset_backend/schedules/controller/EmployeeController.java
+++ src/main/java/com/github/riset_backend/schedules/controller/EmployeeController.java
@@ -3,7 +3,7 @@ package com.github.riset_backend.schedules.controller;
 import com.github.riset_backend.global.config.auth.custom.CustomUserDetails;
 import com.github.riset_backend.schedules.dto.employee.EmployeeDTO;
 import com.github.riset_backend.schedules.dto.employee.ScheduleDTO;
-import com.github.riset_backend.schedules.service.EmployeeService;
+import com.github.riset_backend.schedules.service.EmployeeSchedulesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,18 +18,18 @@ import java.util.List;
 public class EmployeeController {
 
 
-    private final EmployeeService employeeService;
+    private final EmployeeSchedulesService employeeSchedulesService;
 
     @GetMapping
     public ResponseEntity<List<EmployeeDTO>> getAllEmployees(@AuthenticationPrincipal CustomUserDetails user) {
-        List<EmployeeDTO> employees = employeeService.getAllEmployees(user);
+        List<EmployeeDTO> employees = employeeSchedulesService.getAllEmployees(user);
         return ResponseEntity.ok(employees);
     }
 
     @PostMapping("/addEmployees")
     public ScheduleDTO addEmployees(@RequestBody ScheduleDTO scheduleDTO, @AuthenticationPrincipal CustomUserDetails user) {
 
-        return employeeService.addEmployee(user, scheduleDTO);
+        return employeeSchedulesService.addEmployee(user, scheduleDTO);
     }
 
 }
