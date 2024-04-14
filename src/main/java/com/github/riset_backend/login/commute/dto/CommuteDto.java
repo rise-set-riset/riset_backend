79d74e3ea0290e9fff2f3580759e1ef452ad6c53
Min-Seok-Kim <70834609+Min-Seok-Kim@users.noreply.github.com>
2024-04-15 02:43:49 +0900

    fix: commute
    - 출근 위치 추가
    - 출퇴근 조회

--- src/main/java/com/github/riset_backend/login/commute/dto/CommuteDto.java
+++ src/main/java/com/github/riset_backend/login/commute/dto/CommuteDto.java
@@ -1,9 +1,11 @@
 package com.github.riset_backend.login.commute.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+
 
 public record CommuteDto(
                          @JsonFormat(pattern = "yyyy-MM-dd") LocalDate commuteDate,
