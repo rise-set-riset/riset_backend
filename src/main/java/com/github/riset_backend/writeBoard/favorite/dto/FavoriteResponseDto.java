d9021cb50b266bf3e6f2374afbdc25c3a5b8d018
sang hyeok Lee <saing0813@naver.com>
2024-04-15 01:22:45 +0900

    fix: favoriteResponseDto

--- src/main/java/com/github/riset_backend/writeBoard/favorite/dto/FavoriteResponseDto.java
+++ src/main/java/com/github/riset_backend/writeBoard/favorite/dto/FavoriteResponseDto.java
@@ -3,6 +3,7 @@ package com.github.riset_backend.writeBoard.favorite.dto;
 import com.github.riset_backend.file.dto.FileResponseDto;
 import com.github.riset_backend.login.employee.entity.Employee;
 import com.github.riset_backend.writeBoard.board.dto.BoardResponseDto;
+import com.github.riset_backend.writeBoard.board.dto.PostResponseDto;
 import com.github.riset_backend.writeBoard.board.entity.Board;
 import com.github.riset_backend.writeBoard.favorite.entity.Favorite;
 import com.github.riset_backend.writeBoard.reply.dto.ReplyResponseDto;
@@ -22,14 +23,14 @@ import java.util.Map;
 public class FavoriteResponseDto {
 
     private Long id;
-    private Employee employee;
-    private BoardResponseDto board;
     private Integer indexNumber;
+    private Employee user;
+    private PostResponseDto post;
 
     public FavoriteResponseDto(Favorite favorite) {
         this.id = favorite.getFavoriteId();
-        this.employee = favorite.getEmployee();
-        this.board = new BoardResponseDto(favorite.getBoard());
         this.indexNumber = favorite.getIndexNumber();
+        this.user = favorite.getEmployee();
+        this.post = new PostResponseDto(favorite.getBoard());
     }
 }
