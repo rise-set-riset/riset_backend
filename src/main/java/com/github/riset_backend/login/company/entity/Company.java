b04c1b62440c760ead8bcf6eed0fde9208ae53a1
Joojinsung <dev.jinsung1017@gmail.com>
2024-04-14 13:15:17 +0900

    builder.default 추가

--- src/main/java/com/github/riset_backend/login/company/entity/Company.java
+++ src/main/java/com/github/riset_backend/login/company/entity/Company.java
@@ -51,7 +51,7 @@ public class Company extends BaseEntity {
     @Column(name = "mdfr_id")
     private String Mdfr_Id;
 
-
+    @Builder.Default
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "company", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Schedule> companySchedules = new ArrayList<>();
 
