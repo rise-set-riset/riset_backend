300702b8ad8ca548a0c74577a977024a93c4653f
Min-Seok-Kim <70834609+Min-Seok-Kim@users.noreply.github.com>
2024-04-18 03:24:56 +0900

    feat:배포용 swagger

--- src/main/java/com/github/riset_backend/RisetBackendApplication.java
+++ src/main/java/com/github/riset_backend/RisetBackendApplication.java
@@ -1,6 +1,7 @@
 package com.github.riset_backend;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,6 +9,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 
 @SpringBootApplication
 @EnableMongoRepositories
+@OpenAPIDefinition(servers = {@Server(url = "https://dev.risetconstruction.net", description = "Default Server URL")})
 public class RisetBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RisetBackendApplication.class, args);
